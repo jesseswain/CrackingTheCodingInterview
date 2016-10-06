3ba433f3cf80febd73ffea202f91ffb4e78156b8
Jesse Swain <jss7676@gmail.com>
2016-10-05 18:32:05 -0700

    Added isUnique

--- src/com/github/jesseswain/Chapter1.java
+++ src/com/github/jesseswain/Chapter1.java
@@ -1,7 +1,21 @@
 package com.github.jesseswain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Chapter1 {
-	public int tester(){
-		return 1;
+	public boolean isUnique(String str) {
+		if (str == null) {
+			return true;
+		}
+		Set<Character> chars = new HashSet<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			if (chars.contains(str.charAt(i))) {
+				return false;
+			} else {
+				chars.add(str.charAt(i));
+			}
+		}
+		return true;
 	}
 }
--- test/com/github/jesseswain/Chapter1Test.java
+++ test/com/github/jesseswain/Chapter1Test.java
@@ -6,10 +6,23 @@ import org.junit.Test;
 
 public class Chapter1Test {
     Chapter1 chapter1 = new Chapter1();
-
 	@Test
-	public void test() {
-		assertEquals(1, chapter1.tester()); 
+	public void isUniqueShouldFindDuplicates() {
+		assertFalse(chapter1.isUnique("aa"));
+		assertFalse(chapter1.isUnique("abbot"));
+		assertFalse(chapter1.isUnique("bib"));
+		assertFalse(chapter1.isUnique("circular"));
+		assertFalse(chapter1.isUnique("99"));
+		assertFalse(chapter1.isUnique("!!"));
+		
+		assertTrue(chapter1.isUnique("help"));
+		assertTrue(chapter1.isUnique("flask"));
+		assertTrue(chapter1.isUnique("triangle"));
+		assertTrue(chapter1.isUnique("race"));
+		assertTrue(chapter1.isUnique(""));
+		assertTrue(chapter1.isUnique("901"));
+		assertTrue(chapter1.isUnique("#!"));
+		assertTrue(chapter1.isUnique("cC"));
+		assertTrue(chapter1.isUnique(null));
 	}
-
 }
